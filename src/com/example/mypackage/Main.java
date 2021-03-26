289f0979dbb216c22830e3f5e7768439dbe6f1a5
Di Qian <diqian@gmail.com>
2021-03-26 01:06:51 -0400

    binary search tree

---
+++ src/com/example/mypackage/Main.java
@@ -0,0 +1,17 @@
+package com.example.mypackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    BST tree = new BST();
+	    tree.insert(10, "Ten");
+        tree.insert(20, "twenty");
+        tree.insert(15, "fifteen");
+
+        System.out.println(tree.findMin().key);
+        System.out.println(tree.findMax().key);
+
+        System.out.println(tree.remove(10));
+        System.out.println(tree.findMin().key);
+    }
+}
